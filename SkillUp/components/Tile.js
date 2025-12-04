70934c140563b2b80770fdf2dce5640f6cd18a51
steponaskaciulis-max <steponas.kaciulis@gmail.com>
2025-12-04 21:20:34 +0100

    Update Tile.js

--- SkillUp/components/Tile.js
+++ SkillUp/components/Tile.js
@@ -1,1 +1,8 @@
-export default function Tile(){return <div>Tile</div>}
+export default function Tile({ title, description, onClick }) {
+  return (
+    <div className="tile-card" onClick={onClick}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
